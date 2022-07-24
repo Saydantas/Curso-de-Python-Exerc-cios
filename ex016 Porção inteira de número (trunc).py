cbc55f9007c89bf52ec12e9dd080b955f1a44b95
THIAGO DANTAS DIAS <109814439+Saydantas@users.noreply.github.com>
2022-07-24 20:57:52 -0300

    Delete ex016 Porção inteira de número (trunc).py

--- ex016 Porção inteira de número (trunc).py
+++
@@ -1,5 +0,0 @@
-'''Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.'''
-
-from math import trunc
-n = float(input('Digite um número para obter sua porção inteira: '))
-print(f'O número {n} tem a parte inteira {trunc(n)}')
